897c8337576c2281e809c76a1504ce495c63fa61
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-07-24 20:00:12 +0600

    first commit

---
+++ src/app/(commonlayout)/(home)/page.tsx
@@ -0,0 +1,37 @@
+import Todo from '@/components/Home/Todo';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2>home</h2>
+            <Todo></Todo>
+        </div>
+    );
+};
+
+export default page;
+
+
+
+
+// import Banner from '@/components/modules/home/Banner';
+// import Headding from '@/components/modules/home/Headding';
+// import HPreference from '@/components/modules/home/HPreference';
+// import HProviderMeal from '@/components/modules/home/HProviderMeal';
+// import Info from '@/components/modules/home/Info';
+
+
+// const HomePage = () => {
+//     return (
+//         <div>
+//             <Info></Info>
+//             <Headding></Headding>
+//             <Banner></Banner>
+//             <HProviderMeal></HProviderMeal>
+//             <HPreference></HPreference>
+//         </div>
+//     );
+// };
+
+// export default HomePage;
---
+++ src/components/Home/Todo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todo = () => {
+    return (
+        <div>
+            <h2>Todo Form</h2>
+        </div>
+    );
+};
+
+export default Todo;
